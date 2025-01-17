af9bab3085883ba473238284d1f2dd4531a206fd
a monte <ammtz2314@gmail.com>
2025-01-17 20:53:59 +0000

    Final touches

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 // index.js
 
 // init project
-require('dotenv').config();
-var express = require('express');
-var app = express();
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import { join } from 'path';
+
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+// Basic routing (http://expressjs.com/en/starter/basic-routing.html)
+app.get('/', (req, res) => {
+  res.sendFile(join(process.cwd(), 'views', 'index.html'));
 });
 
 // quick implementation...
@@ -28,7 +30,8 @@ app.get('/api/whoami', (req, res) => {
   });
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+// Listen for requests :)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Your app is listening on port ${PORT}`);
 });
